a93b874d5f51d2b2a37b9f733d2077bc8dcf3fe9
ewanhowell5195 <48070995+ewanhowell5195@users.noreply.github.com>
2022-06-26 23:54:36 +0100

    update pack page

--- src/pages/pack/page.js
+++ src/pages/pack/page.js
@@ -15,7 +15,7 @@ class PackPage extends Page {
       return
     }
     const $ = this.$
-    $("#banner-background").css("background-image", `url("/assets/images/resourcepacks/${pack}/banner.webp")`)
+    $("#banner-background").css("background-image", data.banner ? `url("/assets/images/resourcepacks/${pack}/banner.webp")` : `url("/assets/images/resourcepacks/${pack}/images/${data.images[0]}.webp")`)
     $("img#logo").attr("src", `/assets/images/resourcepacks/${pack}/logo.webp`)
     if (resourcepacks.packs[pack].optifine) {
       $("#optifine").removeClass("hidden")
@@ -24,13 +24,26 @@ class PackPage extends Page {
     }
     $("#title").text(data.name ?? pack.replace(/-/g, " ").toTitleCase())
     $("#subtitle").text(data.subtitle)
+    $("#description").text(data.description)
     const images = $("#image-container")
     const imageRow = $("#thumbnail-row")
     for (const [i, image] of data.images.entries()) {
-      images.append(E("img").attr({
+      images.prepend(E("img").attr({
         id: `image-${i}`,
         src: `/assets/images/resourcepacks/${pack}/images/${image}.webp`
-      }).addClass("showcase-image"))
+      }).addClass("showcase-image").on("click", () => {
+        const popup = E("div").addClass("popup-container").append(
+          E("div").addClass("popup-image-container").append(
+            E("img").addClass("popup-image").attr("src", `/assets/images/resourcepacks/${pack}/images/${image}.webp`).css({
+              "max-width": "calc(90vw)",
+              "max-height": "calc(90vh)"
+            }),
+            $("#close-icon").contents().clone(true).addClass("popup-image-close").on("click", () => popup.remove())
+          )
+        ).on("click", evt => {
+          if (evt.target.classList[0] === "popup-container") popup.remove()
+        }).appendTo(document.body)
+      }))
       imageRow.append(E("div").attr("id", `thumbnail-${i}`).addClass("thumbnail-image").css("background-image", `url("/assets/images/resourcepacks/${pack}/images/${image}.webp")`).on("click", () => {
         img = i
         showImage()
