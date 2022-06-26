340c38a94ba952a2ef2a95988be429fc22fe722b
ewanhowell5195 <48070995+ewanhowell5195@users.noreply.github.com>
2022-06-26 19:09:12 +0100

    pack images

--- src/pages/pack/page.js
+++ src/pages/pack/page.js
@@ -22,6 +22,42 @@ class PackPage extends Page {
       if (resourcepacks.packs[pack].optifine === 1) $("#optional").removeClass("hidden")
       else $("#required").removeClass("hidden")
     }
+    $("#title").text(data.name ?? pack.replace(/-/g, " ").toTitleCase())
+    $("#subtitle").text(data.subtitle)
+    const images = $("#image-container")
+    const imageRow = $("#thumbnail-row")
+    for (const [i, image] of data.images.entries()) {
+      images.append(E("img").attr({
+        id: `image-${i}`,
+        src: `/assets/images/resourcepacks/${pack}/images/${image}.webp`
+      }).addClass("showcase-image"))
+      imageRow.append(E("div").attr("id", `thumbnail-${i}`).addClass("thumbnail-image").css("background-image", `url("/assets/images/resourcepacks/${pack}/images/${image}.webp")`).on("click", () => {
+        img = i
+        showImage()
+      }))
+    }
+    let img = 0
+    const prev = $("#prev").on("click", () => {
+      if (prev.hasClass("disabled")) return
+      img--
+      showImage()
+    })
+    const next = $("#next").on("click", () => {
+      if (next.hasClass("disabled")) return
+      img++
+      showImage()
+    })
+    function showImage() {
+      $(".showcase-image").removeClass("shown")
+      $(`#image-${img}`).addClass("shown")
+      $(".thumbnail-image").removeClass("selected")
+      $(`#thumbnail-${img}`).addClass("selected")
+      if (img === 0) prev.addClass("disabled")
+      else prev.removeClass("disabled")
+      if (img === data.images.length - 1) next.addClass("disabled")
+      else next.removeClass("disabled")
+    }
+    showImage()
   }
 }
 
