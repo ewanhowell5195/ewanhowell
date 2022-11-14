128ad81d9d229fd1ed8295016ae8a9993d31e6d4
ewanhowell5195 <48070995+ewanhowell5195@users.noreply.github.com>
2022-11-14 17:35:58 +0000

    param handling for maps, dungeons, and renders

--- src/pages/home/page.js
+++ src/pages/home/page.js
@@ -9,8 +9,12 @@ class HomePage extends Page {
   async setData(params) {
     if (Object.keys(params).length) setTimeout(() => {
       if (params.pack) openPage(new URL(`/resourcepacks/${params.pack}`, location.origin), true)
-      else if (params.map) openPage(new URL(`/maps/${params.map}`, location.origin), true)
       else if (params.resourcepacks !== undefined || params.pack !== undefined) openPage(new URL("/resourcepacks", location.origin), true)
+      else if (params.map) openPage(new URL(`/maps/${params.map}`, location.origin), true)
+      else if (params.maps !== undefined || params.map !== undefined) openPage(new URL("/maps", location.origin), true)
+      else if (params.dungeon) openPage(new URL(`/dungeonsmods/${params.dungeon}`, location.origin), true)
+      else if (params.dungeons !== undefined || params.dungeon !== undefined) openPage(new URL("/dungeonsmods", location.origin), true)
+      else if (params.renders !== undefined) openPage(new URL("/renders", location.origin), true)
     }, 0)
   }
 }
