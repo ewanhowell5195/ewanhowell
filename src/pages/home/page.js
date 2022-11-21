0e3cb7ce966409dc1735b874afa4ee80f99268d9
ewanhowell5195 <48070995+ewanhowell5195@users.noreply.github.com>
2022-11-21 19:48:28 +0000

    openPage bug fix

--- src/pages/home/page.js
+++ src/pages/home/page.js
@@ -8,14 +8,18 @@ export default class HomePage extends Page {
   static tag = "home-page"
 
   async setData(params) {
-    if (Object.keys(params).length) setTimeout(() => {
-      if (params.pack) openPage(new URL(`/resourcepacks/${params.pack}`, location.origin), "replace")
-      else if (params.resourcepacks !== undefined || params.pack !== undefined) openPage(new URL("/resourcepacks", location.origin), "replace")
-      else if (params.map) openPage(new URL(`/maps/${params.map}`, location.origin), "replace")
-      else if (params.maps !== undefined || params.map !== undefined) openPage(new URL("/maps", location.origin), "replace")
-      else if (params.dungeon) openPage(new URL(`/dungeonsmods/${params.dungeon}`, location.origin), "replace")
-      else if (params.dungeons !== undefined || params.dungeon !== undefined) openPage(new URL("/dungeonsmods", location.origin), "replace")
-      else if (params.renders !== undefined) openPage(new URL("/renders", location.origin), "replace")
-    }, 0)
+    if (Object.keys(params).length) {
+      if (params.pack) this.redirect = `/resourcepacks/${params.pack}`
+      else if ("resourcepacks" in params || "pack" in params) this.redirect = "/resourcepacks"
+      else if (params.map) this.redirect = `/maps/${params.map}`
+      else if ("maps" in params || "map" in params) this.redirect = "/maps"
+      else if (params.dungeon) this.redirect = `/dungeonsmods/${params.dungeon}`
+      else if ("dungeons" in params || "dungeon" in params) this.redirect = "/dungeonsmods"
+      else if ("renders" in params) this.redirect = "/renders"
+    }
+  }
+
+  onOpened() {
+    if (this.redirect) openPage(new URL(this.redirect, location.origin), "replace")
   }
 }
