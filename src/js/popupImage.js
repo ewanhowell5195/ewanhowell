f1efaf1cc2a72e6ca4369cd54f6a9e46ce7b07ff
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2023-03-21 11:12:39 +0000

    remove popup no-scroll

--- src/js/popupImage.js
+++ src/js/popupImage.js
@@ -1,18 +1,15 @@
 export function popupImage(url, width = "initial") {
   if (url instanceof HTMLCanvasElement) url = url.toDataURL()
-  const body = $(document.body).addClass("no-scroll")
   const popup = E("div").addClass("popup").append(
     E("div").addClass("popup-container").append(
       E("img").addClass("popup-image").attr("src", url).css("width", width),
       E("img").attr("src", "/../assets/images/svg/close.svg").addClass("popup-image-close").on("click", e => {
         popup.remove()
-        body.removeClass("no-scroll")
       })
     )
   ).on("click", e => {
     if (e.target.classList[0] === "popup") {
       popup.remove()
-      body.removeClass("no-scroll")
     }
   }).appendTo(document.body)
 }
